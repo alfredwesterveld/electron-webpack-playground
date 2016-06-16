34649c236e31f1267a080aff5613d27f75317438
Alfred Westerveld <alfredwesterveld@gmail.com>
2016-06-16 22:40:23 +0200

    first

---
+++ entry.js
@@ -0,0 +1,6 @@
+console.log(process.versions.node);
+console.log('new test');
+
+const a = new Audio('Beep.mp3');
+a.play();
+
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    entry: "./entry.js",
+    target: "electron",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
